51377206b0ce5d7faede6cf89e1387e571c7005d
Diego Torres <torressdiiego@outlook.com>
2020-11-29 18:52:27 -0300

    Update inicio.php

--- view/inicio.php
+++ view/inicio.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
-
     <title><?= $titulo?></title>
 </head>
 <body>
