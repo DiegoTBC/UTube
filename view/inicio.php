fc15068ea24fa7ba4b7b4bab5c71bb1b70ce100f
Diego Torres <torressdiiego@outlook.com>
2020-11-28 22:29:58 -0300

    criação das views de login e cadastro de usuario

---
+++ view/inicio.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="ot-BR">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
+
+    <title><?= $titulo?></title>
+</head>
+<body>
